cbc3680007116250f02d8ce7b9ca099d87f71ede
Javier Gonzalez <javierggt@yahoo.com>
2024-02-12 14:13:21 -0500

    step 1: a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
-export default function Square() {
-  return <button className="square">X</button>;
+function Square() {
+  return (
+    <button className="square">X</button>
+  );
+}
+
+export default function App() {
+  return <Square/>;
 }
