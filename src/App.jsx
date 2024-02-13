c86fd847d5d07a15452883ebe83ed8102eb5f1e5
Javier Gonzalez <javierggt@yahoo.com>
2024-02-12 21:56:02 -0500

    step 5: lifting state

--- src/App.jsx
+++ src/App.jsx
@@ -29,23 +29,19 @@ function Square(props) {
 }
 
 
-function Board() {
-
-  const [next, setNext] = useState("X");
-  const [squares, setSquares] = useState(Array(9).fill(null));
+function Board(props) {
 
   function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
+    if (props.squares[i] || calculateWinner(props.squares)) {
       return;
     }
-    let newSquares = squares.slice();
-    newSquares[i] = next;
-    setSquares(newSquares);
-    setNext(next === "X" ? "O" : "X");
+    let newSquares = props.squares.slice();
+    newSquares[i] = props.next;
+    props.handleClick(newSquares)
   }
 
   let status;
-  const winner = calculateWinner(squares);
+  const winner = calculateWinner(props.squares);
   if (winner) {
     status = "Winner: " + winner;
   }
@@ -53,19 +49,19 @@ function Board() {
   return (
     <div>
       <div className="board-row">
-        <Square onClick={() => handleClick(0)} value={squares[0]}/>
-        <Square onClick={() => handleClick(1)} value={squares[1]}/>
-        <Square onClick={() => handleClick(2)} value={squares[2]}/>
+        <Square onClick={() => handleClick(0)} value={props.squares[0]}/>
+        <Square onClick={() => handleClick(1)} value={props.squares[1]}/>
+        <Square onClick={() => handleClick(2)} value={props.squares[2]}/>
       </div>
       <div className="board-row">
-        <Square onClick={() => handleClick(3)} value={squares[3]}/>
-        <Square onClick={() => handleClick(4)} value={squares[4]}/>
-        <Square onClick={() => handleClick(5)} value={squares[5]}/>
+        <Square onClick={() => handleClick(3)} value={props.squares[3]}/>
+        <Square onClick={() => handleClick(4)} value={props.squares[4]}/>
+        <Square onClick={() => handleClick(5)} value={props.squares[5]}/>
       </div>
       <div className="board-row">
-        <Square onClick={() => handleClick(6)} value={squares[6]}/>
-        <Square onClick={() => handleClick(7)} value={squares[7]}/>
-        <Square onClick={() => handleClick(8)} value={squares[8]}/>
+        <Square onClick={() => handleClick(6)} value={props.squares[6]}/>
+        <Square onClick={() => handleClick(7)} value={props.squares[7]}/>
+        <Square onClick={() => handleClick(8)} value={props.squares[8]}/>
       </div>
       <div className="status">{status}</div>
     </div>
@@ -73,5 +69,13 @@ function Board() {
 }
 
 export default function App() {
-  return <Board/>;
+  const [next, setNext] = useState("X");
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(newSquares) {
+    setSquares(newSquares);
+    setNext(next === "X" ? "O" : "X");
+  }
+
+  return <Board squares={squares} next={next} handleClick={handleClick}/>;
 }
